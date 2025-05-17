b65a906d7f829593d5a3be89ef205303d5be4e26
hseung127 <hseung127@gmail.com>
2025-05-17 21:00:30 +0900

    Spring Boot 프로젝트 파일 추가

---
+++ src/main/java/com/example/aimindroute/AimindrouteApplication.java
@@ -0,0 +1,13 @@
+package com.example.aimindroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimindrouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimindrouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aimindroute/AimindrouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aimindroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AimindrouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
